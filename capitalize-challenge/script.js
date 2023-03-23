e6aff8981c708a7d364c18cd3634e13d6a11e513
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-03-23 20:17:09 +0100

    Add files via upload

---
+++ capitalize-challenge/script.js
@@ -0,0 +1,4 @@
+const myString = 'developer';
+let myNewString = myString.charAt(0).toUpperCase() + myString.slice(1);
+
+console.log(myNewString);
---
+++ number-challenge/script.js
@@ -0,0 +1,15 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+let sumOutput = `sumOutput: ${x} + ${y} = ${x + y}`
+let differenceOutput = `differenceOutput: ${x} - ${y} = ${x - y}`
+let productOutput = `productOutput: ${x} * ${y} = ${x * y}`
+let quotientOutput = `quotientOutput: ${x} / ${y} = ${x / y}`
+let rmOutput = `rmOutput: ${x} % ${y} = ${x % y}`
+
+
+
+console.log(sumOutput);
+console.log(differenceOutput);
+console.log(productOutput);
+console.log(rmOutput);
